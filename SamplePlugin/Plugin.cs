f8e8c21e0854f74a3f3140368b9ceabeb6b1d916
De <78926263+DeDumChee@users.noreply.github.com>
2024-08-01 17:15:17 -0400

    Repo rename
    First pass rename / metadata edit from SamplePlugin to PFN

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -1,12 +1,12 @@
-﻿using Dalamud.Game.Command;
+using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
-using SamplePlugin.Windows;
+using PFNotifier.Windows;
 
-namespace SamplePlugin;
+namespace PFNotifier;
 
 public sealed class Plugin : IDalamudPlugin
 {
@@ -14,11 +14,11 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
 
-    private const string CommandName = "/pmycommand";
+    private const string CommandName = "/pfn";
 
     public Configuration Configuration { get; init; }
 
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
+    public readonly WindowSystem WindowSystem = new("PFNotifier");
     private ConfigWindow ConfigWindow { get; init; }
     private MainWindow MainWindow { get; init; }
 
